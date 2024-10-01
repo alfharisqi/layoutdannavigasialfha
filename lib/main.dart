7ebd766c5cc28b0ad451f2be7634eaef6199a699
alfhaberdasi <risqiwicaksonoalfha.com>
2024-10-01 10:23:03 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout: Nama dan NIM Anda',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
         ),
       ),
     );
